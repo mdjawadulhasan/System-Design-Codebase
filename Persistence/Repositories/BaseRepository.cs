947250b972e07e8f71db526520c77bbdef36ee9d
mdjawadulhasan <jawadul.hasan@brainstation-23.com>
2024-12-07 01:25:32 +0600

    Clean Architechture Folder structure done

---
+++ Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Persistence;
+using TicketManagement.Application.Contracts.Persistence;
+
+namespace TicketManagement.Persistence.Repositories;
+
+public class BaseRepository<T> : IAsyncRepository<T> where T : class
+{
+    protected readonly SystemDesignDbContext _dbContext;
+
+    public BaseRepository(SystemDesignDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public async Task<T> GetByIdAsync(Guid id)
+    {
+        return await _dbContext.Set<T>().FindAsync(id);
+    }
+
+    public async Task<IReadOnlyList<T>> ListAllAsync()
+    {
+        return await _dbContext.Set<T>().ToListAsync();
+    }
+
+    public async Task<T> AddAsync(T entity)
+    {
+        await _dbContext.Set<T>().AddAsync(entity);
+        await _dbContext.SaveChangesAsync();
+
+        return entity;
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        _dbContext.Entry(entity).State = EntityState.Modified;
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(T entity)
+    {
+        _dbContext.Set<T>().Remove(entity);
+        await _dbContext.SaveChangesAsync();
+    }
+}
---
+++ Persistence/SystemDesignDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class SystemDesignDbContext:DbContext
+    {
+    }
+}
